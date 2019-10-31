dd55e19845bd88bd414b0a6503ad82154270b444
John Whitaker <john.whitaker@staples.com>
2019-10-31 19:50:39 -0400

    Initial commit

---
+++ ApiGetConsole/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace ApiGetConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //https://jsonplaceholder.typicode.com/guide.html - Test API
+
+            bool useCredentials = false;
+            //GET from API
+            string url = @"https://jsonplaceholder.typicode.com/posts/1/comments";            
+            var result = GetFromApi(url, useCredentials);
+            Console.WriteLine(result);
+
+            //post to API
+            result = string.Empty;
+            url = @"https://jsonplaceholder.typicode.com/posts";
+            result = PostToApi(url, useCredentials);
+            Console.WriteLine(result);
+        }
+
+        public static string GetFromApi(string url, bool useCredentials)
+        {
+            WebRequest request = WebRequest.Create(url);
+            request.Method = "Get";
+            if (useCredentials)
+            {
+                request.Credentials = new NetworkCredential("USERNAME", "PASSWORD");
+            }
+            HttpWebResponse response = null;
+            response = (HttpWebResponse)request.GetResponse();
+            string strResult = string.Empty;
+            using (Stream stream = response.GetResponseStream())
+            {
+                StreamReader sr = new StreamReader(stream);
+                strResult = sr.ReadToEnd();
+                sr.Close();
+            }
+            return strResult;
+        }
+
+        public static string PostToApi(string url, bool useCredentials)
+        {
+            WebRequest request = WebRequest.Create(url);
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            if (useCredentials)
+            {
+                request.Credentials = new NetworkCredential("USERNAME", "PASSWORD");
+            }
+            HttpWebResponse response = null;
+            string postData = "{\"title\": \"Foo\",\"body\":\"test body\", \"userId\":\"50\"}";
+            string strResult = string.Empty;
+            using (var sw = new StreamWriter(request.GetRequestStream()))
+            {
+                sw.Write(postData);
+                sw.Flush();
+                sw.Close();
+
+                response = (HttpWebResponse)request.GetResponse();
+                using(var sr = new StreamReader(response.GetResponseStream()))
+                {
+                    strResult = sr.ReadToEnd();
+                    sr.Close();
+                }
+            }
+            return strResult;
+        }
+
+
+
+    }
+}
